88fdfb57d32487b82503f16b3d430bdc3ffcaaab
Sara Elwakil <83132057+SaraElwakill@users.noreply.github.com>
2025-09-25 19:02:20 +0000

    add ID

--- m3dattak-next/src/components/ProductCard.tsx
+++ m3dattak-next/src/components/ProductCard.tsx
@@ -11,7 +11,7 @@ interface ProductCardProps {
 
 const WHATSAPP_NUMBER = "201119201970"; 
 
-function ProductCard ({ imageUrl, Name,  Description,badge }: ProductCardProps) {
+function ProductCard ({ imageUrl, Name,  Description,badge,ID }: ProductCardProps) {
 
 
    const getWhatsappLink = (): string => {
